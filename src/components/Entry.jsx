aa592f01d7725062a38ea42a22bff602c59e9b62
Ana Freitas <ana.freitaas16@gmail.com>
2022-08-23 12:25:29 -0300

    created Entry function to add the props

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -4,20 +4,26 @@ import emojipedia from "../emojipedia";
 
 function Entry(props) {
 
-    <dl className="dictionary">
+    return (
+
+        <dl className="dictionary">
+
         <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              {props.emoji}
-            </span>
-            <span>Tense Biceps</span>
-          </dt>
-          <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
-          </dd>
+
+            <dt>
+
+                <span className="emoji" role="img" aria-label="Tense Biceps"> {props.emoji} </span>
+                <span> {props.name} </span>
+
+            </dt>
+
+            <dd> {props.meaning} </dd>
         </div>
-        </dl> 
-}
+
+    </dl>
+
+    );
+    
+};
 
 export default Entry; 
