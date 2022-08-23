480c0979222e8a46288327826605a7177c7d83ba
Ana Freitas <ana.freitaas16@gmail.com>
2022-08-23 12:00:53 -0300

    creating react components

---
+++ src/components/Entry.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import emojipedia from "../emojipedia";
+
+
+function Entry(props) {
+
+    <dl className="dictionary">
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              {props.emoji}
+            </span>
+            <span>Tense Biceps</span>
+          </dt>
+          <dd>
+            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
+            used in connection with doing sports, e.g. at the gym.
+          </dd>
+        </div>
+        </dl> 
+}
+
+export default Entry; 
