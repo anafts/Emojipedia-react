34d8d6d69ef163c17564c42a2e16d74316ea281a
Ana Freitas <ana.freitaas16@gmail.com>
2022-09-22 16:18:49 -0300

    adding emojis

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -116,6 +116,126 @@ const emojipedia = [
     meaning: 
     "A sneezing face with eyes closed, blowing its nose into a handkerchief. I caught a cold! Shows that someone is ill or feels uncomfortable."
   },
+
+  {
+    id:16 ,
+    emoji: "🤒",
+    name: " Face With Thermometer" ,
+    meaning: 
+    "The sad face with the thermometer in the mouth expresses illness. Caught a flu or other illness that is associated with fever. Or is worried about getting sick."
+  },
+
+  {
+    id:17 ,
+    emoji: "🤑",
+    name: "Money-Mouth Face",
+    meaning: 
+    "Smiley has dollar signs in its eyes, the tongue is a banknote. Could have won something or is sensing a financial chance. Implicates a sense of wealth."
+  },
+
+  {
+    id:18 ,
+    emoji: "😧",
+    name: " Anguished Face" ,
+    meaning: 
+    "Something unexpected happened! Face with raised eyebrows, open eyes and open mouth. A mixture of shock and disappointment. Represents fear, frustration, horror and unexpected, negative surprises."
+  },
+
+  {
+    id:19 ,
+    emoji: "😶‍🌫",
+    name: "Face in clouds" ,
+    meaning: 
+    "This is nebulous! The face is surrounded by fog or clouds. Those who have their heads in the clouds are either dreamy or do not want to admit reality. The view of something is blurred. There may also be a state of confusion or bliss."
+  },
+
+  {
+    id:20 ,
+    emoji: "🤩",
+    name: " Smirking Face With Starry Eyes" ,
+    meaning: 
+    "Is very excited about something and already full of anticipation. Overwhelmed and speechless after meeting someone you like a lot. Fascinated by the glory and the world of the stars."
+  },
+
+  {
+    id:21 ,
+    emoji: "😙",
+    name: "Kissing Face With Smiling Eyes" ,
+    meaning: 
+    "Relaxed face expression, smiling eyes and kissing lips. A kiss to friends or “We hug and kiss you“ to relatives. Can also be used for whistling."
+  },
+
+  {
+    id:22 ,
+    emoji: "🤓",
+    name: "Nerd Face" ,
+    meaning: 
+    "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+
+  {
+    id:23 ,
+    emoji: "🤢",
+    name: " Nauseated Face" ,
+    meaning: 
+    "Disgusted smiley, which is already green with sickness and nausea. Might mean disgust, reluctance, or aversion, or stand for sickness."
+  },
+
+  {
+    id:24 ,
+    emoji: "🤐",
+    name: "Zipper-Mouth Face" ,
+    meaning: 
+    "Smiley with zipper instead of a mouth is keeping a secret for itself. Lips are closed or chat partner's lips should remain closed. Can also mean silence because you cannot find the right words."
+  },
+
+  {
+    id:25 ,
+    emoji: "😷",
+    name: "Face With Medical Mask" ,
+    meaning: 
+    "The mask is for protection. Either to protect yourself or others from infection. Someone is in the hospital, has to go to the doctor or has caught a disease. Wearing the masks is widely spread in Asia."
+  },
+
+  {
+    id:26 ,
+    emoji: "🥱",
+    name: "Yawning face" ,
+    meaning: 
+    "I am dog-tired! The eyes are closed and the hand covers a yawn. The night was too short, you couldn't get enough sleep. Indication that a topic or a person is not of interest, but rather boring."
+  },
+
+  {
+    id:27 ,
+    emoji: "😬",
+    name: "Grimacing Face" ,
+    meaning: 
+    "This is really embarrassing for me now! Expression of nervousness, awkwardness or embarrassment. Scared and guilty smile: Someone has done something stupid and tries to resolve the tense situation by grimacing."
+  },
+
+  {
+    id:28 ,
+    emoji: "😶",
+    name: "Face Without Mouth" ,
+    meaning: 
+    "Emoji without a mouth. Is speechless, has no words. Someone does not want to or cannot say anything about a specific topic. Used in difficult, embarrassing or bad conversations. Imagine what it would say if it had a mouth!"
+  },
+
+  {
+    id:29 ,
+    emoji: "😑",
+    name: "Expressionless Face" ,
+    meaning: 
+    "Face without any emotional expression, with closed eyes and mouth. Tired, annoyed, emotionless, no patience, the end of the flagpole is reached. The right words are missing to continue a conversation."
+  },
+
+  {
+    id:30 ,
+    emoji: "🤕",
+    name: " Face With Head Bandage" ,
+    meaning: 
+    "Sad looking face with head bandage: Someone is ill, had an accident, or is in hospital. The bandage symbolizes health problems."
+  },
 ];
 
 export default emojipedia;
